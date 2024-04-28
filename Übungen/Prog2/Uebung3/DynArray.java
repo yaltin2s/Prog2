2c2cd65d7987867d6ead0ca7e7a5fd5c266411cd
Yousef Altinawi <Yousef.Altinawi@smail.inf.h-brs.de>
2024-04-28 23:04:16 +0200

    prog2

--- Übungen/Prog2/Uebung3/DynArray.java
+++ Übungen/Prog2/Uebung3/DynArray.java
@@ -2,11 +2,11 @@ package Uebung3;
 
 
 public class DynArray<T> {
-
-    private static int startGroesse = 2; // Laenge des Arrays (initial)
+    private static int startGroesse = 2; // Länge des Arrays (initial)
 
     private T[] array;
-    private int size; // Anzahl der "belegten" Plaetze
+    private int size; // Anzahl der "belegten" Plätze
+    private int capacity;
 
 
     @SuppressWarnings("unchecked")
@@ -24,7 +24,7 @@ public class DynArray<T> {
     }
 
     public void set(int pos, T e) throws IndexOutOfBoundsException {
-        // Aendern von bereits belegten Positionen
+        // Ändern von bereits belegten Positionen
         if (pos < 0 || pos >= size)
             throw new IndexOutOfBoundsException();
         array[pos]= e;
@@ -37,7 +37,7 @@ public class DynArray<T> {
     }
 
     public void add(T e) {
-        // hinzufuegen neuer Elemente
+        // hinzufügen neuer Elemente
         if(size >= array.length)
             increase();
         array[size++] = e;
@@ -52,7 +52,7 @@ public class DynArray<T> {
 
     @SuppressWarnings("unchecked")
     private void increase() {
-        // Verlaengerung des Arrays bei Bedarf
+        // Verlängerung des Arrays bei Bedarf
         T[] neu = (T[]) new Object[array.length*2];
         for (int i = 0; i < size; i++)
             neu[i] = array[i];
@@ -60,7 +60,7 @@ public class DynArray<T> {
     }
 
 
-    // nur fuer Testzwecke
+    // nur für Testzwecke
     protected void show() {
         System.out.println("size\t length\t Array-Inhalt");
         System.out.print(size + "\t" + array.length + "\t");
@@ -85,4 +85,35 @@ public class DynArray<T> {
         remove(0);
     }
 
+    // c) void delete(T e)
+    public void delete(T e) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(e)) {
+                remove(i);
+                break;
+            }
+        }
+    }
+
+    // d) boolean contains(T e)
+    public boolean contains(T e) {
+        for (T element : array) {
+            if (element.equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // e) void decrease()
+    @SuppressWarnings("unchecked")
+    private void decrease() {
+        if (size <= array.length / 4) {
+            int newCapacity = Math.max ( capacity, array.length / 2 );
+            T[] newArray = (T[]) new Object[newCapacity];
+            System.arraycopy ( array, 0, newArray, 0, size );
+            array = newArray;
+        }
+
+    }
 }
